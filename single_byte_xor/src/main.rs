463bfcf607f5309f85d2b5784ff9f1cb78eadca8
Robert Gaulin <robert.gaulin@libertymutual.com>
2016-09-29 16:07:30 -0400

    Finished Challenge 3

--- single_byte_xor/src/main.rs
+++ single_byte_xor/src/main.rs
@@ -1,7 +1,42 @@
 extern crate rustc_serialize as serialize;
 
 use serialize::hex::FromHex;
+use std::str;
 
 fn main() {
-    let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let max_char = 128;
+
+    let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".from_hex().unwrap();
+    let mut best_decoded_string = std::string::String::new();
+    let mut best_xor_char = 0;
+
+    for xor_char in 0..max_char {
+        let mut xor_vec = Vec::new();
+        for byte in hex.iter() {
+            xor_vec.push(byte ^ xor_char);
+        }
+
+        let decoded_str = str::from_utf8(&xor_vec).unwrap();
+        if compare_string(best_decoded_string.to_string(), decoded_str.to_string()) == true {
+            best_decoded_string = decoded_str.to_string();
+            best_xor_char = xor_char;
+        }
+    }
+
+    println!("{}: {}", best_xor_char, best_decoded_string);
+}
+
+fn score_string(s: &str) -> usize {
+    return s.matches(char::is_alphabetic).count() + s.matches(' ').count();
+}
+
+fn compare_string<'a>(s1: std::string::String, s2: std::string::String) -> bool {
+    let s1_score = score_string(&s1);
+    let s2_score = score_string(&s2);
+
+    if s1_score > s2_score {
+        return false;
+    } else {
+        return true;
+    }
 }
